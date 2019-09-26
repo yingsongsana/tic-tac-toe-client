597cfae212cd38586501775d600afd2946a0e680
yingsongsana <yingsongsana@gmail.com>
2019-09-26 15:58:59 -0400

    Added a way to keep track of current player.
    Keeping track of player and displaying message on whose turn it is.
    Would like to figure out how to combine user message and invalid move
    message, and have them replace each other as opposed to being underneath
    each other

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,13 +5,26 @@ const ui = require('./ui.js')
 const gameBoard = ['', '', '', '', '', '', '', '', '']
 
 // keep track of player
-let player = 'x'
+let play = 'x'
 // swap player
-const swapPlayer = function () {
-  if (player === 'x') {
-    player = 'o'
+const swapPlay = function () {
+  if (play === 'x') {
+    play = 'o'
   } else {
-    player = 'x'
+    play = 'x'
+  }
+}
+
+const playerX = 'x'
+const playerO = 'o'
+let currentPlayer = playerX
+const findCurrentPlayer = function () {
+  if (currentPlayer === playerX) {
+    currentPlayer = playerO
+    ui.currentPlayerOMessage()
+  } else {
+    currentPlayer = playerX
+    ui.currentPlayerXMessage()
   }
 }
 
@@ -19,12 +32,13 @@ const add = function (event) {
   const click = event.target
   if ($(click).text() === '') {
     const clickId = $(click).prop('id')
-    gameBoard[clickId] = player
-    $(click).html(player)
-    swapPlayer()
+    gameBoard[clickId] = play
+    $(click).html(play)
+    swapPlay()
   } else {
     ui.invalidMoveMessage()
   }
+  findCurrentPlayer()
 }
 
 module.exports = {
--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -1,9 +1,28 @@
 'use strict'
 
+const userMessage = function (newText) {
+  $('#game-message').text(newText)
+  $('#game-massage').removeClass('alt_alert')
+  $('#game-massage').addClass('alert')
+}
+const alertMessage = function (newText) {
+  $('#alert-message').text(newText)
+}
+
 const invalidMoveMessage = function () {
-  $('#game-message').text('Invalid move! Pick another!')
+  alertMessage('Invalid move! Pick another!')
+}
+
+const currentPlayerXMessage = function () {
+  userMessage(`Make your move player X `)
+}
+
+const currentPlayerOMessage = function () {
+  userMessage('Make your move player O')
 }
 
 module.exports = {
-  invalidMoveMessage
+  invalidMoveMessage,
+  currentPlayerXMessage,
+  currentPlayerOMessage
 }
